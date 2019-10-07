9fbbf76e2f24763460f3076e79e235734e65aa88
Jean-michel Gambard <jgambard@z1r2p2.le-101.fr>
2019-10-07 17:52:32 +0200

    Add ft_strlcpy.c / Modif ft_strncmp.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,22 +1,35 @@
 /* ************************************************************************** */
 /*                                                          LE - /            */
 /*                                                              /             */
-/*   ft_strncmp.c                                     .::    .:/ .      .::   */
+/*   ft_strlcpy.c                                     .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: jgambard <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2019/10/07 17:45:56 by jgambard     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/07 17:50:20 by jgambard    ###    #+. /#+    ###.fr     */
+/*   Created: 2019/10/07 17:49:05 by jgambard     #+#   ##    ##    #+#       */
+/*   Updated: 2019/10/07 17:51:34 by jgambard    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-int		ft_strncmp(const char *s1, const char *s2, size_t n)
+unsigned int	ft_strlcpy(char *dest, const char *src, size_t size)
 {
-	if ((*s1 || *s2) && (*s1 == *s2 && n && n - 1))
-		return (ft_strncmp(++s1, ++s2, --n));
-	else
-		return (n ? (unsigned char)*s1 - (unsigned char)*s2 : 0);
+	unsigned int		len;
+	unsigned int		i;
+
+	len = 0;
+	while (src[len])
+		len++;
+	i = 0;
+	if (size)
+	{
+		while (src[i] && (i < size - 1))
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = 0;
+	}
+	return (len);
 }
