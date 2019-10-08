9a4fd3aad02b86e6dd635e12b2445fc8abd3c536
Jean-michel Gambard <jgambard@z1r2p2.le-101.fr>
2019-10-08 16:23:44 +0200

    Add ft_lstclear.c

---
+++ ft_lstclear.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_lstclear.c                                    .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: jgambard <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/10/08 16:12:54 by jgambard     #+#   ##    ##    #+#       */
+/*   Updated: 2019/10/08 16:21:59 by jgambard    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	if ((*lst)->next)
+		ft_lstclear(&(*lst)->next, del);
+	(*del)((*lst)->content);
+	free(*lst);
+	*lst = 0;
+}
