2e8e65623f519844a25798741c387f761d57b483
Jean-michel Gambard <jgambard@z1r2p2.le-101.fr>
2019-10-08 11:13:00 +0200

    Add ft_strjoin.c

---
+++ ft_strjoin.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_strjoin.c                                     .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: jgambard <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/10/08 11:03:16 by jgambard     #+#   ##    ##    #+#       */
+/*   Updated: 2019/10/08 11:12:16 by jgambard    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*join;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (s1[i])
+		i++;
+	j = 0;
+	while (s2[j])
+		j++;
+	if (!(join = malloc(sizeof(*join) * (i + j + 1))))
+		return (0);
+	i = -1;
+	while (s1[++i])
+		join[i] = s1[i];
+	j = -1;
+	while (s2[++j])
+		join[i + j] = s2[j];
+	join[i + j] = 0;
+	return (join);
+}
