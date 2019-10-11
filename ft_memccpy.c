44dd2a122c5c9235af141fe38e01a76a9a2c63e4
Jean-michel Gambard <jgambard@z1r2p2.le-101.fr>
2019-10-11 17:30:01 +0200

    Fix ft_memccpy.c (cast unsigned char)

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: jgambard <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/07 14:57:25 by jgambard     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/07 15:20:15 by jgambard    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/11 17:26:40 by jgambard    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,12 +18,12 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 	unsigned int	i;
 
 	i = 0;
-	while (i < n && (unsigned char)c != ((char*)src)[i])
+	while (i < n && (unsigned char)c != (unsigned char)((char*)src)[i])
 	{
-		((char*)dst)[i] = ((char*)src)[i];
+		((char*)dst)[i] = (unsigned char)((char*)src)[i];
 		i++;
 	}
 	if (i < n)
-		((char*)dst)[i] = ((char*)src)[i];
+		((char*)dst)[i] = (unsigned char)((char*)src)[i];
 	return ((unsigned char)c != ((char*)src)[i] ? 0 : dst + i + 1);
 }
