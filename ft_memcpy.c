951e9769e95e6c0719ffe08e52170292fc08e1c6
Jean-michel Gambard <jgambard@z1r2p2.le-101.fr>
2019-10-11 17:22:23 +0200

    Fix ft_memcpy.c (behavior with both null pointer and add cast unsigned char)

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: jgambard <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/07 14:44:53 by jgambard     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/07 14:55:18 by jgambard    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/11 17:19:43 by jgambard    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,7 +18,9 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	unsigned int	i;
 
 	i = -1;
+	if (!dst && !src)
+		return (dst);
 	while (++i < n)
-		((char*)dst)[i] = ((char*)src)[i];
+		((char*)dst)[i] = (unsigned char)((char*)src)[i];
 	return (dst);
 }
