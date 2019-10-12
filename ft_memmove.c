7b69fb9d421392dfd3228b988df032226b6b06a4
Jean-michel Gambard <jgambard@z1r2p2.le-101.fr>
2019-10-12 10:45:55 +0200

    Fix ft_memmove.c (remove tmp buffer)

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: jgambard <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/07 15:31:00 by jgambard     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/11 17:33:27 by jgambard    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/12 10:42:11 by jgambard    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -16,15 +16,14 @@
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	size_t				i;
-	char				tmp[len];
 
 	if (!len || (!dst && !src))
 		return (dst);
-	i = -1;
-	while (++i < len)
-		tmp[i] = ((char*)src)[i];
-	while (--i > 0)
-		((char*)dst)[i] = tmp[i];
-	((char*)dst)[i] = tmp[i];
+	i = (dst > src ? len - 1 : 0);
+	while (len--)
+	{
+		((char*)dst)[i] = ((char*)src)[i];
+		i += (dst > src ? -1 : 1);
+	}
 	return (dst);
 }
