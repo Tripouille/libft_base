da3b1b16efa1c2469f93ccfc5cbc86bc983db87b
Jean-michel Gambard <jgambard@z1r2p2.le-101.fr>
2019-10-17 10:40:36 +0200

    Secure ft_lstadd_back_bonus

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jgambard <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/08 15:12:45 by jgambard     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/08 15:15:26 by jgambard    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/17 10:38:45 by jgambard    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,8 +15,8 @@
 
 void	ft_lstadd_back(t_list **alst, t_list *new)
 {
-	if (*alst)
+	if (alst && *alst)
 		ft_lstadd_back(&(*alst)->next, new);
-	else
+	else if (alst)
 		*alst = new;
 }
